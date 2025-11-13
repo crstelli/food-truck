4c6a4599d4e18b8e131a12670b35bc3dece96caf
Giuseppe <cresctelli@gmail.com>
2025-11-13 20:31:55 +0100

    Implemented shadecn/ui

--- app/(pages)/app/(components)/(sidebar)/(menus)/Search.tsx
+++ app/(pages)/app/(components)/(sidebar)/(menus)/Search.tsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
 import { useSidebarContext } from "../useSidebarContext";
 
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { MapPin } from "lucide-react";
+
 function Search() {
   const [typing, setTyping] = useState("");
   const { places } = useSidebarContext();
 
   return (
     <div>
-      <h1 className="text-3xl font-bold">Search for a place</h1>
-      <input
+      <h1 className="text-3xl font-bold text-center text-gray-800">
+        Search for a place
+      </h1>
+      <Input
         type="text"
         value={typing}
         onChange={(e) => setTyping(e.target.value)}
-        className="border border-orange-500"
+        className=" ring-orange-300 mb-4 focus:border-orange-500 mt-2 border-gray-400"
       />
 
       {typing.length > 2 &&
@@ -21,14 +34,40 @@ function Search() {
             place.name.toLowerCase().includes(typing.toLowerCase())
           )
           .map((place) => (
-            <div
+            // <div
+            //   key={place.id}
+            //   className="mt-10 grid grid-cols-2 border border-orange-500 text-center"
+            // >
+            //   <h2>{place.name}</h2>
+            //   <h3>Ratings: {place.rating_value}</h3>
+            //   <button className="col-span-2">Locate</button>
+            // </div>
+
+            <Card
               key={place.id}
-              className="mt-10 grid grid-cols-2 border border-orange-500 text-center"
+              className="border border-gray-400 flex flex-col gap-1"
             >
-              <h2>{place.name}</h2>
-              <h3>Ratings: {place.rating_value}</h3>
-              <button className="col-span-2">Locate</button>
-            </div>
+              <CardHeader>
+                <CardTitle className="text-xl font-semibold">
+                  {place.name}
+                </CardTitle>
+              </CardHeader>
+
+              <CardContent>
+                <div className="flex items-center gap-2">
+                  <span className="font-medium text-gray-900">
+                    {place.rating_value} ⭐
+                  </span>
+                </div>
+              </CardContent>
+
+              <CardFooter>
+                <Button className="border border-gray-400 flex items-center gap-1">
+                  <MapPin className="w-4 h-4" />
+                  Mostra sulla mappa
+                </Button>
+              </CardFooter>
+            </Card>
           ))}
     </div>
   );
