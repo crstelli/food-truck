2e8c6226e8493aa8dd118b4d021ea2a72023fa79
Giuseppe <cresctelli@gmail.com>
2025-11-14 08:43:10 +0100

    Improved info page and reviews

--- app/(pages)/app/(components)/(sidebar)/(menus)/Info.tsx
+++ app/(pages)/app/(components)/(sidebar)/(menus)/Info.tsx
@@ -1,7 +1,9 @@
 import { useSearchParams } from "next/navigation";
 import { useSidebarContext } from "../useSidebarContext";
 
-import { Button } from "@/app/(components)/Button";
+import { Button } from "@/components/ui/button";
+import { getAffordabilityColor } from "@/app/(lib)/getAffordabilityColor";
+import { Star } from "lucide-react";
 
 function Info() {
   const searchParams = useSearchParams();
@@ -12,24 +14,48 @@ function Info() {
   if (!place) return <p>No Place</p>;
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold">{place.name}</h1>
-      <h2>Affordability: {place.affordability}</h2>
-      <span>Added: {new Date(place.created_at).toLocaleString()}</span>
-      <h3>Raings: {place.rating_value}</h3>
-      <Button>Get Direction</Button>
+    <>
+      <div className="grid grid-cols-2 gap-4">
+        <h1 className="text-3xl col-span-2 text-center">{place.name}</h1>
+        <div>
+          <h2 className="font-bold">Affordability</h2>
+          <span className={getAffordabilityColor(place.affordability)}>
+            {place.affordability}
+          </span>
+        </div>
+        <div>
+          <h2 className="font-bold">Added</h2>
+          <span className="text-gray-600">
+            {new Date(place.created_at).toLocaleString()}
+          </span>
+        </div>
+        <div>
+          <h2 className="font-bold">Rating</h2>
+          <span className="flex items-center gap-1">
+            <Star size={20} className="text-yellow-400 fill-yellow-400" />
+            {place.rating_value} / 5
+          </span>
+        </div>
+        <Button className="self-center">Get Direction</Button>
+      </div>
 
-      <h2 className="mt-10">Reviews</h2>
+      <h2 className="mt-10 text-center text-xl">Last Reviews</h2>
       <div className="flex flex-col gap-2 overflow-auto">
         {place.reviews.map((rev) => (
-          <div key={rev.id} className="grid bg-orange-200 p-2">
-            <h3>{rev.user}</h3>
-            <span>Rating: {rev.rating}</span>
-            <span>{rev.content}</span>
+          <div
+            key={rev.id}
+            className="grid grid-cols-2 p-2 border border-border rounded-md"
+          >
+            <h3 className="font-bold">{rev.user}</h3>
+            <span className="flex items-center gap-1">
+              {rev.rating}
+              <Star size={20} className="text-yellow-400 fill-yellow-400" />
+            </span>
+            <span className="col-span-2 mt-2 text-gray-600">{rev.content}</span>
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
 }
 
